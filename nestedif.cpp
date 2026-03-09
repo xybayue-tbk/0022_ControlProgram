4c98b047ddc40f77f9a9007674e7699f4ad4343e
xybayue-tbk <xybayue@gmail.com>
2026-03-09 11:10:22 +0700

    02.cpp

---
+++ nestedif.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+float Rata_rata(float a, float b)
+{
+    return (a+b) / 2;
+}
+
+string Status_lulus(float r)
+{
+    if (r >= 60)
+        return "lolos";
+    else
+        return "gagal";
+}
+
+
